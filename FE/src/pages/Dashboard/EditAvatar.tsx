18aa2779ed839fafc89eabeaf6bd3db46f601d11
Varun Sahni <varunsahni@tamu.edu>
2024-12-26 13:50:54 +0530

    backend fixed for jpeg

--- FE/src/pages/Dashboard/EditAvatar.tsx
+++ FE/src/pages/Dashboard/EditAvatar.tsx
@@ -3,24 +3,51 @@ import ReactImagePickerEditor from 'react-image-picker-editor';
 import 'react-image-picker-editor/dist/index.css'
 
 const EditAvatar = ({ imageSrc, set_imageSrc, userId }: any) => {
+    // const handleImageChange = async (newDataUri: any) => {
+    //     // Convert base64 to file
+    //     const base64Response = await fetch(newDataUri);
+    //     const blob = await base64Response.blob();
+    //     const file = new File([blob], `${userId}.png`, { type: 'image/png' });
+    //
+    //     // Create form data
+    //     const formData = new FormData();
+    //     formData.append('image', file);
+    //
+    //     try {
+    //         const response = await fetch('http://localhost:5000/api/image/upload', {
+    //             method: 'POST',
+    //             body: formData
+    //         });
+    //
+    //         const data = await response.json();
+    //         set_imageSrc(data.data.url); // Assuming the API returns the image URL
+    //     } catch (error) {
+    //         console.error('Error uploading image:', error);
+    //     }
+    // };
     const handleImageChange = async (newDataUri: any) => {
+        // Extract file extension from the data URI
+        const fileExtension = newDataUri.split(';')[0].split('/')[1];
+        console.log(newDataUri.split(';')[0].split(':')[1]);
+
+
         // Convert base64 to file
         const base64Response = await fetch(newDataUri);
         const blob = await base64Response.blob();
-        const file = new File([blob], `${userId}.png`, { type: 'image/png' });
+        const file = new File([blob], `${userId}.${fileExtension}`, { type: blob.type });
 
         // Create form data
         const formData = new FormData();
         formData.append('image', file);
+        console.log("formData", formData);
 
         try {
             const response = await fetch('http://localhost:5000/api/image/upload', {
                 method: 'POST',
                 body: formData
             });
-
             const data = await response.json();
-            set_imageSrc(data.data.url); // Assuming the API returns the image URL
+            set_imageSrc(data.data.url);
         } catch (error) {
             console.error('Error uploading image:', error);
         }
