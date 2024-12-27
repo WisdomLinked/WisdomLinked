25ebfa3f436e744f685bb9b2b9db8609de368c46
Varun Sahni <varunsahni@tamu.edu>
2024-12-28 02:24:28 +0530

    turn server fix

--- BE/turn.js
+++ BE/turn.js
@@ -1,12 +1,29 @@
+// var Turn = require('node-turn');
+// var server = new Turn({
+//   // set options
+//  // listeningIp: '0.0.0.0',
+//   listeningPort: 3478,
+//  // relayIps: '0.0.0.0',
+//   authMech: 'long-term',
+//   credentials: {
+//     username: "password"
+//   }
+// });
+//
+// console.log("TURN server is starting...");
+// server.on('error', (error) => {
+//   console.error("TURN server encountered an error:", error);
+// });
+// server.start();
+// console.log("TURN server started on port 3478.");
+
 var Turn = require('node-turn');
 var server = new Turn({
-  // set options
- // listeningIp: '0.0.0.0',
+  // Set options
   listeningPort: 3478,
- // relayIps: '0.0.0.0',
   authMech: 'long-term',
   credentials: {
-    username: "password"
+    "efA389S6BJFSNKYQP2": "dkvSztjG5Rs60Er0", // Updated username and password
   }
 });
 
