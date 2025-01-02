891cef1ae6badb6e4fb2085ec6ceed2b8902ea57
Varun Sahni <varunsahni@tamu.edu>
2025-01-02 09:57:19 +0530

    code cleaned

--- FE/src/pages/Dashboard/Messenger/Messages/ProfileModal.tsx
+++ FE/src/pages/Dashboard/Messenger/Messages/ProfileModal.tsx
@@ -111,37 +111,3 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ isOpen, onClose, userDetail
 };
 
 export default ProfileModal;
-
-//     return (
-//         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
-//             <div ref={modalRef} className="w-96 p-6 bg-[#1a1a24] rounded-lg shadow-2xl border border-gray-800">
-//                 <h2 className="text-xl font-semibold mb-4 text-gray-100 border-b border-gray-800 pb-3">Profile
-//                     Information</h2>
-//                 <div className="space-y-4 bg-[#20202c] p-4 rounded-md">
-//                     <p className="text-gray-300">
-//                         <strong className="text-gray-200">Name:</strong> {userDetails.username}
-//                     </p>
-//                     <p className="text-gray-300">
-//                         <strong className="text-gray-200">Email:</strong> {userDetails.email}
-//                     </p>
-//                     <p className="text-gray-300">
-//                         <strong className="text-gray-200">Country:</strong> {userDetails?.country?.name}
-//                     </p>
-//                     <p className="text-gray-300">
-//                         <strong className="text-gray-200">Timezone:</strong> {timezone}
-//                     </p>
-//                 </div>
-//                 <button
-//                     onClick={handleCloseClick}
-//                     className="mt-6 w-full px-4 py-2.5 bg-[#2d2d3d] text-gray-200 rounded-lg hover:bg-[#363649] transition-all duration-200"
-//                 >
-//                     Close
-//                 </button>
-//             </div>
-//         </div>
-//
-//
-//     );
-// };
-//
-// export default ProfileModal;
