dc0a2831da41d8812453469c3e10ad0e635771e0
Varun Sahni <varunsahni@tamu.edu>
2025-01-05 00:41:48 -0500

    video restored

--- FE/src/socket/webRTC.ts
+++ FE/src/socket/webRTC.ts
@@ -34,14 +34,13 @@ export const checkLocalAudioVideoStreams = async () => {
 }
 
 export const getLocalStreamPreview = async (audioOnly: boolean, callback?: () => void, room?: boolean, failedCallback?: (err: any) => any) => {
-    console.log("Getting local stream preview");
 
     const {videoStream, audioStream} = await getLocalStream()
     const constraints = room ?
         { audio: audioStream ? true : false, video: videoStream ? true : false} :
         { audio: true, video: audioOnly ? false : true };
 
-    console.log("Media constraints:", constraints);
+    console.log("constraints", constraints);
 
     store.dispatch({
         type: actionTypes.setLocalStreamAvailability,
@@ -51,9 +50,20 @@ export const getLocalStreamPreview = async (audioOnly: boolean, callback?: () =>
         }
     })
 
+    // FOR VIRTUAL STREAM -------------
+    // if (room) {
+    //     store.dispatch(setLocalStreamRoom(true) as any);
+    // } else {
+    //     store.dispatch(setLocalStream(true) as any);
+    // }
+
+    // if (callback) {
+    //     callback();
+    // }
+    // return
+
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
         if (room) {
-            console.log("Local stream obtained:", stream);
             store.dispatch(setLocalStreamRoom(stream) as any);
         } else {
             store.dispatch(setLocalStream(stream) as any);
@@ -78,48 +88,74 @@ export const getLocalStreamPreview = async (audioOnly: boolean, callback?: () =>
     })
 }
 
+
 const peerConfiguration = () => {
-    console.log("Configuring ICE servers");
 
-    const stunServers = [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:3478" },
-        { urls: "stun:stun2.l.google.com:3478" },
-        { urls: "stun:stun3.l.google.com:3478" },
-        { urls: "stun:stun4.l.google.com:3478" },
-    ];
-
-    const turnServers = process.env.REACT_APP_TURN_URL
-        ? [
-            {
-                urls: `turn:${process.env.REACT_APP_TURN_URL}:3478`, // TURN over UDP
-                username: "efA389S6BJFSNKYQP2", // Your TURN username
-                credential: "dkvSztjG5Rs60Er0", // Your TURN password
-            },
-            {
-                urls: `turn:${process.env.REACT_APP_TURN_URL}:80?transport=tcp`, // TURN over TCP
-                username: "efA389S6BJFSNKYQP2",
-                credential: "dkvSztjG5Rs60Er0",
-            },
-        ]
-        : [];
-
-    return {
-        iceServers: [...stunServers, ...turnServers],
-    };
+    if (process.env.REACT_APP_TURN_URL) {
+        console.log("Using only TURN server");
+        return {
+            iceServers: [
+                {
+                    urls: [
+                        `turn:${process.env.REACT_APP_TURN_URL}:3478?transport=udp`,
+                        `turn:${process.env.REACT_APP_TURN_URL}:80?transport=tcp`
+                    ],
+                    username: "username1",
+                    credential: "password1"
+                }
+            ]
+        }
+    } else {
+        console.log("Using only STUN server");
+        return {
+            iceServers: [
+                {
+                    urls: "stun:stun.l.google.com:19302",
+                },
+            ],
+        };
+    }
 };
 
+//peerConfiguration - turn and stun
+// const peerConfiguration = () => {
+//     console.log("Configuring ICE servers");
+//
+//     const stunServers = [
+//         { urls: "stun:stun.l.google.com:19302" },
+//         { urls: "stun:stun.l.google.com:5349" },
+//     ];
+//
+//     const turnServers = process.env.REACT_APP_TURN_URL
+//         ? [
+//             {
+//                 urls: [
+//                     `turn:${process.env.REACT_APP_TURN_URL}:3478?transport=udp`,
+//                     `turn:${process.env.REACT_APP_TURN_URL}:80?transport=tcp`
+//                 ],
+//                 username: "efA389S6BJFSNKYQP2",
+//                 credential: "dkvSztjG5Rs60Er0"
+//             }
+//         ]
+//         : [];
+//
+//     return {
+//         iceServers: [...stunServers, ...turnServers],
+//     };
+// };
+
 export const newPeerConnection = (initiator: boolean) => {
+
     const stream = store.getState().videoChat.localStream
-    console.log("Creating new peer connection. Initiator:", initiator, "Stream:", stream);
 
     if (!stream) {
-        console.error("No local stream available. Cannot create peer connection.");
         throw new Error("No local stream");
+
     }
 
-    const configuration = peerConfiguration();
+    console.log("from web ", stream);
 
+    const configuration = peerConfiguration();
     const peer = new Peer({
         initiator: initiator,
         trickle: false,
@@ -127,21 +163,18 @@ export const newPeerConnection = (initiator: boolean) => {
         stream: stream,
     });
 
-    console.log("Peer connection created:", peer);
+
+
     return peer;
 }
+
 
 let peers: any = {};
 
 export const prepareNewPeerConnection = (connUserSocketId: string, isInitiator: boolean) => {
-    console.log("Preparing new peer connection. Initiator:", isInitiator, "Socket ID:", connUserSocketId);
+    // connUserSocketId; -> who has joined the room
 
     const localStream = store.getState().room.localStreamRoom;
-
-    if (!localStream) {
-        console.warn("No local stream available. Skipping peer preparation.");
-        return;
-    }
 
     if (isInitiator) {
         console.log("preparing new peer connection as initiator");
@@ -149,7 +182,13 @@ export const prepareNewPeerConnection = (connUserSocketId: string, isInitiator: 
         console.log("preparing new peer connection as not initiator");
     }
 
-    console.log("Using local stream for peer connection:", localStream);
+    // if(!localStream) {
+    //     return
+    // }
+
+    console.log("localStream", localStream)
+
+    console.log("hello")
 
     peers[connUserSocketId] = new Peer({
         initiator: isInitiator,
@@ -158,14 +197,19 @@ export const prepareNewPeerConnection = (connUserSocketId: string, isInitiator: 
     });
 
     peers[connUserSocketId].on("signal", (data: Peer.SignalData) => {
-        console.log("Generated signaling data for connection:", data);
-        const signalData = {signal: data, connUserSocketId: connUserSocketId,};
+        const signalData = {
+            signal: data,
+            connUserSocketId: connUserSocketId,
+        };
+
         signalPeerData(signalData);
     });
 
     peers[connUserSocketId].on("stream", (remoteStream: any) => {
         // TODO
-        console.log("Received remote stream from peer:", connUserSocketId, remoteStream);
+        // add new remote stream (of connUserSocketId who has joined the room) to our server store
+        console.log("remote stream came from other user");
+        console.log("direct connection has been established");
         remoteStream.connUserSocketId = connUserSocketId;
         addNewRemoteStream(remoteStream);
     });
@@ -175,18 +219,15 @@ export const handleSignalingData = (data: {
     connUserSocketId: string;
     signal: Peer.SignalData;
 }) => {
-    console.log("Handling incoming signaling data:", data);
     const { connUserSocketId, signal } = data;
 
     if (peers[connUserSocketId]) {
-        console.log("Adding signaling data to peer connection:", connUserSocketId);
         peers[connUserSocketId].signal(signal);
-    } else {
-        console.warn("Peer connection not found for:", connUserSocketId);
     }
 };
 
 const addNewRemoteStream = (remoteStream: MediaStream | Boolean) => {
+    console.log("Hi")
     const remoteStreams = store.getState().room.remoteStreams;
     const newRemoteStreams = [...remoteStreams, remoteStream];
 
@@ -212,6 +253,7 @@ export const handleParticipantLeftRoom = (data: { connUserSocketId: string }) =>
     }
 
     const remoteStreams = store.getState().room.remoteStreams;
+
     const newRemoteStreams = remoteStreams.filter(
         (remoteStream) =>
             (remoteStream as any).connUserSocketId !== connUserSocketId
@@ -239,3 +281,246 @@ export const switchOutgoingTracks = (stream: MediaStream) => {
         }
     }
 };
+
+
+// import Peer from "simple-peer";
+// import { setLocalStreamRoom, setRemoteStreams } from "../actions/roomActions";
+// import { setLocalStream } from "../actions/videoChatActions";
+// import { store } from "../store";
+// import { signalPeerData } from "./socketConnection";
+// import { actionTypes } from "../actions/types";
+// import { showAlert } from "../actions/alertActions";
+//
+// const getLocalStream = async () => {
+//     let videoStream, audioStream;
+//     try {
+//         videoStream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+//     } catch (error) {
+//     }
+//     try {
+//         audioStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+//     } catch (error) {
+//     }
+//     return {
+//         videoStream,
+//         audioStream
+//     }
+// }
+//
+// export const checkLocalAudioVideoStreams = async () => {
+//     const {videoStream, audioStream} = await getLocalStream()
+//     store.dispatch({
+//         type: actionTypes.setLocalStreamAvailability,
+//         payload: {
+//             videoStream,
+//             audioStream
+//         }
+//     })
+// }
+//
+// export const getLocalStreamPreview = async (audioOnly: boolean, callback?: () => void, room?: boolean, failedCallback?: (err: any) => any) => {
+//     console.log("Getting local stream preview");
+//
+//     const {videoStream, audioStream} = await getLocalStream()
+//     const constraints = room ?
+//         { audio: audioStream ? true : false, video: videoStream ? true : false} :
+//         { audio: true, video: audioOnly ? false : true };
+//
+//     console.log("Media constraints:", constraints);
+//
+//     store.dispatch({
+//         type: actionTypes.setLocalStreamAvailability,
+//         payload: {
+//             videoStream,
+//             audioStream
+//         }
+//     })
+//
+//     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+//         if (room) {
+//             console.log("Local stream obtained:", stream);
+//             store.dispatch(setLocalStreamRoom(stream) as any);
+//         } else {
+//             store.dispatch(setLocalStream(stream) as any);
+//         }
+//
+//         if (callback) {
+//             callback();
+//         }
+//
+//     }).catch((err) => {
+//         console.log(err);
+//         console.log("Error getting local stream");
+//         store.dispatch(showAlert(
+//             room ?
+//                 "You don't have any media devices, plesae check your microphone and camera" :
+//                 audioOnly ?
+//                     "You don't have any audio devices, plesae check your microphone" :
+//                     "You don't have any video devices, plesae check your camera"
+//         ))
+//         if (failedCallback)
+//             failedCallback(err)
+//     })
+// }
+//
+// const peerConfiguration = () => {
+//     console.log("Configuring ICE servers");
+//
+//     const stunServers = [
+//         { urls: "stun:stun.l.google.com:19302" },
+//         { urls: "stun:stun1.l.google.com:3478" },
+//         { urls: "stun:stun2.l.google.com:3478" },
+//         { urls: "stun:stun3.l.google.com:3478" },
+//         { urls: "stun:stun4.l.google.com:3478" },
+//     ];
+//
+//     const turnServers = process.env.REACT_APP_TURN_URL
+//         ? [
+//             {
+//                 urls: `turn:${process.env.REACT_APP_TURN_URL}:3478`, // TURN over UDP
+//                 username: "efA389S6BJFSNKYQP2", // Your TURN username
+//                 credential: "dkvSztjG5Rs60Er0", // Your TURN password
+//             },
+//             {
+//                 urls: `turn:${process.env.REACT_APP_TURN_URL}:80?transport=tcp`, // TURN over TCP
+//                 username: "efA389S6BJFSNKYQP2",
+//                 credential: "dkvSztjG5Rs60Er0",
+//             },
+//         ]
+//         : [];
+//
+//     return {
+//         iceServers: [...stunServers, ...turnServers],
+//     };
+// };
+//
+// export const newPeerConnection = (initiator: boolean) => {
+//     const stream = store.getState().videoChat.localStream
+//     console.log("Creating new peer connection. Initiator:", initiator, "Stream:", stream);
+//
+//     if (!stream) {
+//         console.error("No local stream available. Cannot create peer connection.");
+//         throw new Error("No local stream");
+//     }
+//
+//     const configuration = peerConfiguration();
+//
+//     const peer = new Peer({
+//         initiator: initiator,
+//         trickle: false,
+//         config: configuration,
+//         stream: stream,
+//     });
+//
+//     console.log("Peer connection created:", peer);
+//     return peer;
+// }
+//
+// let peers: any = {};
+//
+// export const prepareNewPeerConnection = (connUserSocketId: string, isInitiator: boolean) => {
+//     console.log("Preparing new peer connection. Initiator:", isInitiator, "Socket ID:", connUserSocketId);
+//
+//     const localStream = store.getState().room.localStreamRoom;
+//
+//     if (!localStream) {
+//         console.warn("No local stream available. Skipping peer preparation.");
+//         return;
+//     }
+//
+//     if (isInitiator) {
+//         console.log("preparing new peer connection as initiator");
+//     } else {
+//         console.log("preparing new peer connection as not initiator");
+//     }
+//
+//     console.log("Using local stream for peer connection:", localStream);
+//
+//     peers[connUserSocketId] = new Peer({
+//         initiator: isInitiator,
+//         config: peerConfiguration(),
+//         stream: localStream!,
+//     });
+//
+//     peers[connUserSocketId].on("signal", (data: Peer.SignalData) => {
+//         console.log("Generated signaling data for connection:", data);
+//         const signalData = {signal: data, connUserSocketId: connUserSocketId,};
+//         signalPeerData(signalData);
+//     });
+//
+//     peers[connUserSocketId].on("stream", (remoteStream: any) => {
+//         // TODO
+//         console.log("Received remote stream from peer:", connUserSocketId, remoteStream);
+//         remoteStream.connUserSocketId = connUserSocketId;
+//         addNewRemoteStream(remoteStream);
+//     });
+// };
+//
+// export const handleSignalingData = (data: {
+//     connUserSocketId: string;
+//     signal: Peer.SignalData;
+// }) => {
+//     console.log("Handling incoming signaling data:", data);
+//     const { connUserSocketId, signal } = data;
+//
+//     if (peers[connUserSocketId]) {
+//         console.log("Adding signaling data to peer connection:", connUserSocketId);
+//         peers[connUserSocketId].signal(signal);
+//     } else {
+//         console.warn("Peer connection not found for:", connUserSocketId);
+//     }
+// };
+//
+// const addNewRemoteStream = (remoteStream: MediaStream | Boolean) => {
+//     const remoteStreams = store.getState().room.remoteStreams;
+//     const newRemoteStreams = [...remoteStreams, remoteStream];
+//
+//     store.dispatch(setRemoteStreams(newRemoteStreams) as any);
+// };
+//
+// export const closeAllConnections = () => {
+//     Object.entries(peers).forEach((mappedObject) => {
+//         const connUserSocketId = mappedObject[0];
+//         if (peers[connUserSocketId]) {
+//             peers[connUserSocketId].destroy();
+//             delete peers[connUserSocketId];
+//         }
+//     });
+// };
+//
+// export const handleParticipantLeftRoom = (data: { connUserSocketId: string }) => {
+//     const { connUserSocketId } = data;
+//
+//     if (peers[connUserSocketId]) {
+//         peers[connUserSocketId].destroy();
+//         delete peers[connUserSocketId];
+//     }
+//
+//     const remoteStreams = store.getState().room.remoteStreams;
+//     const newRemoteStreams = remoteStreams.filter(
+//         (remoteStream) =>
+//             (remoteStream as any).connUserSocketId !== connUserSocketId
+//     );
+//
+//     store.dispatch(setRemoteStreams(newRemoteStreams) as any);
+// };
+//
+// export const switchOutgoingTracks = (stream: MediaStream) => {
+//     for (let socket_id in peers) {
+//         for (let index in peers[socket_id].streams[0].getTracks()) {
+//             for (let index2 in stream.getTracks()) {
+//                 if (
+//                     peers[socket_id].streams[0].getTracks()[index].kind ===
+//                     stream.getTracks()[index2].kind
+//                 ) {
+//                     peers[socket_id].replaceTrack(
+//                         peers[socket_id].streams[0].getTracks()[index],
+//                         stream.getTracks()[index2],
+//                         peers[socket_id].streams[0]
+//                     );
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// };
