379411ac1d716efc2bcab64796c2ee72c13ceffd
Varun Sahni <varunsahni@tamu.edu>
2025-01-09 00:49:39 +0530

    incoming call audio added

--- FE/src/components/IncomingCall.tsx
+++ FE/src/components/IncomingCall.tsx
@@ -1,97 +1,123 @@
-import React from 'react'
-import { styled } from "@mui/system";
-import Backdrop from '@mui/material/Backdrop';
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
-import PhoneDisabledIcon from "@mui/icons-material/PhoneDisabled";
-import VideocamIcon from "@mui/icons-material/Videocam";
-import { useAppSelector } from '../store';
-import { callResponse } from '../socket/socketConnection';
+    import React, {useEffect, useRef} from 'react'
+    import { styled } from "@mui/system";
+    import Backdrop from '@mui/material/Backdrop';
+    import Typography from "@mui/material/Typography";
+    import IconButton from "@mui/material/IconButton";
+    import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+    import PhoneDisabledIcon from "@mui/icons-material/PhoneDisabled";
+    import VideocamIcon from "@mui/icons-material/Videocam";
+    import { useAppSelector } from '../store';
+    import { callResponse } from '../socket/socketConnection';
 
+    const MainContainer = styled("div")({
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        padding: "15px 20px",
+        borderRadius: "30px",
+    });
 
-const MainContainer = styled("div")({
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    padding: "15px 20px",
-    borderRadius: "30px",
-});
+    const IncomingCall = () => {
+        const callRequest = useAppSelector(state => state.videoChat.callRequest);
+        const audioRef = useRef<HTMLAudioElement>(null);
 
+        const handleCall = (accepted: boolean, audioOnly: boolean) => {
+            // Stop the ringing sound
+            if (audioRef.current) {
+                audioRef.current.pause();
+                audioRef.current.currentTime = 0; // Reset to the start
+            }
 
+            // Respond to the call
+            callResponse({
+                callerId: callRequest!.callerUserId,
+                callerName: callRequest!.callerName,
+                accepted,
+                audioOnly
+            });
+        };
 
-const IncomingCall = () => {
-    const callRequest = useAppSelector(state => state.videoChat.callRequest);
+        useEffect(() => {
+            // Play the sound on repeat when a call request is detected
+            if (callRequest?.callerUserId && audioRef.current) {
+                audioRef.current.play().catch((err) => {
+                    console.error("Error playing the audio:", err);
+                });
+            } else if (!callRequest?.callerUserId && audioRef.current) {
+                // Stop the sound if no call is active
+                audioRef.current.pause();
+                audioRef.current.currentTime = 0;
+            }
+        }, [callRequest]);
 
-    const handleCall = (accepted: boolean, audioOnly: boolean) => {
-        callResponse({
-            callerId: callRequest!.callerUserId,
-            callerName: callRequest!.callerName,
-            accepted,
-            audioOnly
-        });
-    };
+        return (
+            <Backdrop
+                sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+                open={!!callRequest?.callerUserId}
+            >
+                {/* Audio element for the ringing sound */}
+                <audio ref={audioRef} loop preload="auto">
+                    <source
+                        src="https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3"
+                        type="audio/mp3"
+                    />
+                    Your browser does not support the audio element.
+                </audio>
+                <MainContainer>
+                    <Typography
+                        sx={{
+                            color: "black",
+                            marginBottom: "3px",
+                            fontSize: "16px",
+                            fontWeight: "bold",
+                        }}
+                    >
+                        Incoming {callRequest?.audioOnly ? "audio" : "video"} call from{" "}
+                        {callRequest?.callerName}
+                    </Typography>
 
-    return (
-        <Backdrop
-            sx={{
-                color: "#fff",
-                zIndex: (theme) => theme.zIndex.drawer + 1,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-            open={!!callRequest?.callerUserId}
-        >
-            <MainContainer>
-                <Typography
-                    sx={{
-                        color: "black",
-                        marginBottom: "3px",
-                        fontSize: "16px",
-                        fontWeight: "bold",
-                    }}
-                >
-                    Incoming {callRequest?.audioOnly ? "audio" : "video"} call from{" "}
-                    {callRequest?.callerName}
-                </Typography>
+                    <div>
+                        {!callRequest?.audioOnly && (
+                            <IconButton
+                                color="success"
+                                onClick={() => {
+                                    handleCall(true, false);
+                                }}
+                            >
+                                <VideocamIcon />
+                            </IconButton>
+                        )}
 
-                <div>
-                    {!callRequest?.audioOnly && (
                         <IconButton
                             color="success"
                             onClick={() => {
-                                handleCall(true, false);
+                                handleCall(true, true);
                             }}
                         >
-                            <VideocamIcon />
+                            <PhoneInTalkIcon />
                         </IconButton>
-                    )}
 
-                    <IconButton
-                        color="success"
-                        onClick={() => {
-                            handleCall(true, true);
-                        }}
-                    >
-                        <PhoneInTalkIcon />
-                    </IconButton>
+                        <IconButton
+                            color="error"
+                            onClick={() => {
+                                handleCall(false, true);
+                            }}
+                        >
+                            <PhoneDisabledIcon />
+                        </IconButton>
+                    </div>
+                </MainContainer>
+            </Backdrop>
+        );
+    }
 
-                    <IconButton
-                        color="error"
-                        onClick={() => {
-                            handleCall(false, true);
-                        }}
-                    >
-                        <PhoneDisabledIcon />
-                    </IconButton>
-                </div>
-            </MainContainer>
-        </Backdrop>
-    );
-}
-
-export default IncomingCall
+    export default IncomingCall
