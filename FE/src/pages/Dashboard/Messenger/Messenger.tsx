b12258aa8e2a1305f787e05f13ef4369cb186909
Varun Sahni <varunsahni@tamu.edu>
2025-02-14 19:53:35 -0600

    message duplicacy resolved

--- FE/src/pages/Dashboard/Messenger/Messenger.tsx
+++ FE/src/pages/Dashboard/Messenger/Messenger.tsx
@@ -1,5 +1,36 @@
+// import React, { useEffect } from "react";
+// import { styled } from "@mui/system";
+// import { useAppSelector } from "../../../store";
+// import WelcomeMessage from "./WelcomeMessage";
+// import ChatDetails from "./ChatDetails";
+// import { setChosenGroupChatDetails } from "../../../actions/chatActions";
+// import { useDispatch } from "react-redux";
+//
+// const Messenger = () => {
+//   const {chat: { chosenChatDetails, chosenGroupChatDetails }, friends: { groupChatList }} = useAppSelector((state) => state);
+//   const dispatch = useDispatch()
+//   useEffect(() => {
+//     if (chosenGroupChatDetails) {
+//       const selectedGroupChat:any = groupChatList.find((x: any) => x.groupId === chosenGroupChatDetails.groupId)
+//       if (selectedGroupChat) {
+//         console.log(selectedGroupChat, 'UPDATED GROUP CHAT DETAIL')
+//         dispatch(setChosenGroupChatDetails(selectedGroupChat))
+//       }
+//     }
+//   }, [groupChatList])
+//
+//   return (
+//     <div className="w-full h-full flex">
+//       <div className="w-full flex flex-grow bg-darkgrey-1">
+//         {chosenChatDetails?.userId || chosenGroupChatDetails?.groupId ?  <ChatDetails/> : <WelcomeMessage/>}
+//       </div>
+//     </div>
+//   );
+// };
+//
+// export default Messenger;
+
 import React, { useEffect } from "react";
-import { styled } from "@mui/system";
 import { useAppSelector } from "../../../store";
 import WelcomeMessage from "./WelcomeMessage";
 import ChatDetails from "./ChatDetails";
@@ -7,25 +38,42 @@ import { setChosenGroupChatDetails } from "../../../actions/chatActions";
 import { useDispatch } from "react-redux";
 
 const Messenger = () => {
-  const {chat: { chosenChatDetails, chosenGroupChatDetails }, friends: { groupChatList }} = useAppSelector((state) => state);
-  const dispatch = useDispatch()
+  // Extract relevant data from the store
+  const {
+    chat: { chosenChatDetails, chosenGroupChatDetails },
+    friends: { groupChatList }
+  } = useAppSelector((state) => state);
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    if (chosenGroupChatDetails) {
-      const selectedGroupChat:any = groupChatList.find((x: any) => x.groupId === chosenGroupChatDetails.groupId)
-      if (selectedGroupChat) {
-        console.log(selectedGroupChat, 'UPDATED GROUP CHAT DETAIL')
-        dispatch(setChosenGroupChatDetails(selectedGroupChat))
-      }
+    if (!chosenGroupChatDetails) return;
+
+    const selectedGroupChat: any = groupChatList.find(
+        (x: any) => x.groupId === chosenGroupChatDetails.groupId
+    );
+
+    if (
+        selectedGroupChat &&
+        selectedGroupChat._id !== chosenGroupChatDetails._id
+    ) {
+      console.log(selectedGroupChat, "UPDATED GROUP CHAT DETAIL");
+      dispatch(setChosenGroupChatDetails(selectedGroupChat));
     }
-  }, [groupChatList])
+  }, [groupChatList, chosenGroupChatDetails, dispatch]);
 
   return (
-    <div className="w-full h-full flex">
-      <div className="w-full flex flex-grow bg-darkgrey-1">
-        {chosenChatDetails?.userId || chosenGroupChatDetails?.groupId ?  <ChatDetails/> : <WelcomeMessage/>}
+      <div className="w-full h-full flex">
+        <div className="w-full flex flex-grow bg-darkgrey-1">
+          {chosenChatDetails?.userId || chosenGroupChatDetails?.groupId ? (
+              <ChatDetails />
+          ) : (
+              <WelcomeMessage />
+          )}
+        </div>
       </div>
-    </div>
   );
 };
 
 export default Messenger;
+
